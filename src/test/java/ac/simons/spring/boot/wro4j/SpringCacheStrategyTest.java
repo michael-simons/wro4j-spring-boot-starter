7a7e1fd95d8186c91a9aaf52469c33b398d394ed
Drummond Dawson <drumonii@users.noreply.github.com>
2018-10-31 22:48:48 -0400

    Use new MockitoJUnitRunner

--- src/test/java/ac/simons/spring/boot/wro4j/SpringCacheStrategyTest.java
+++ src/test/java/ac/simons/spring/boot/wro4j/SpringCacheStrategyTest.java
@@ -23,8 +23,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.runners.MockitoJUnitRunner;
 
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 
